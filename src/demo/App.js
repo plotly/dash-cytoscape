57787e385c9992f0b5ee8613744cda9027c69957
Xing Han Lu <xhlperso@gmail.com>
2018-08-08 12:06:44 -0400

    Added cytoscape to package, App.js example now working

--- src/demo/App.js
+++ src/demo/App.js
@@ -1,7 +1,7 @@
 /* eslint no-magic-numbers: 0 */
 import React from 'react';
 import ReactDOM from 'react-dom';
-import CytoscapeComponent from 'cytoscape-reactjs';
+import CytoscapeComponent from '../react-cytoscapejs/src/component.js';
 
 class App extends React.Component {
   constructor(props){
@@ -10,13 +10,20 @@ class App extends React.Component {
 
   render(){
     const elements = [
-       { data: { id: 'one', label: 'Node 1' }, position: { x: 0, y: 0 } },
-       { data: { id: 'two', label: 'Node 2' }, position: { x: 100, y: 0 } },
+       { data: { id: 'one', label: 'Node 1' }, position: { x: 50, y: 50 } },
+       { data: { id: 'two', label: 'Node 2' }, position: { x: 200, y: 200 } },
        { data: { source: 'one', target: 'two', label: 'Edge from Node1 to Node2' } }
     ];
 
-    return <CytoscapeComponent elements={elements} style="width: 600px; height: 600px;" />;
+    return <CytoscapeComponent
+        elements={elements}
+        style={{
+            width: '1000px',
+            height: '600px',
+            margin: '50px'
+        }}
+    />;
   }
-};
+}
 
 export default App;
