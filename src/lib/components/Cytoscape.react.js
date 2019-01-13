02c37b547ec026af709d33c2fb39735de4355cd0
Xing Han Lu <xhlperso@gmail.com>
2019-01-13 16:43:52 -0500

    Extensively Updated Docstring for Proptypes
    Added references to Cytoscape.js docs, as well as a description of all
    items for the element and style dictionaries in bullet points. Gave an
    overview of items for tapNode and tapEdge dictionaries

--- src/lib/components/Cytoscape.react.js
+++ src/lib/components/Cytoscape.react.js
@@ -278,14 +278,18 @@ export default class Cytoscape extends Component {
 
     render() {
         const {
+            // HTML attribute props
             id,
             style,
             className,
+            // Common props
             elements,
             stylesheet,
             layout,
+            // Viewport Manipulation
             pan,
             zoom,
+            // Viewport Mutability and gesture Toggling
             panningEnabled,
             userPanningEnabled,
             minZoom,
@@ -326,75 +330,132 @@ export default class Cytoscape extends Component {
 
 
 Cytoscape.propTypes = {
+    // HTML attribute props
+
     /**
-     * The ID used to identify this component in Dash callbacks
+     * The ID used to identify this component in Dash callbacks.
      */
     id: PropTypes.string,
 
     /**
-     * Html Class of the component
+     * Sets the class name of the element (the value of an element's html
+     * class attribute).
      */
     className: PropTypes.string,
 
     /**
-     * Add inline styles to the root element
+     * Add inline styles to the root element.
      */
     style: PropTypes.object,
 
+    // Dash specific props
+
     /**
      * Dash-assigned callback that should be called whenever any of the
-     * properties change
+     * properties change.
      */
     setProps: PropTypes.func,
 
+    // Common props
+
     /**
-     * The flat list of Cytoscape elements to be included in the graph, each
-     * represented as non-stringified JSON.
+     * A list of dictionaries representing the elements of the networks. Each
+     * dictionary describes an element, and specifies its purpose. Keys
+     * accepted are:
+     * - `group` (string): Either 'nodes' or 'edges'. If not given, it's automatically inferred.
+     * - `data` (dictionary): Element specific data.
+     *      - `id` (string): Reference to the element, useful for selectors and edges. Randomly assigned if not given.
+     *      - `label` (string): Optional name for the element, useful when `data(label)` is given to a style's `content` or `label`. It is only a convention.
+     *      - `parent` (string): Only for nodes. Optional reference to another node. Needed to create compound nodes.
+     *      - `source` (string): Only for edges. The id of the source node, which is where the edge starts.
+     *      - `target` (string): Only for edges. The id of the target node, where the edge ends.
+     * - `position` (dictionary): Only for nodes. The position of the node.
+     *      - `x` (number): The x-coordinate of the node.
+     *      - `y` (number): The y-coordinate of the node.
+     * - `selected` (boolean): If the element is selected upon initialisation.
+     * - `selectable` (boolean): If the element can be selected.
+     * - `locked` (boolean): Only for nodes. If the position is immutable.
+     * - `grabbable` (boolean): Only for nodes. If the node can be grabbed and moved by the user.
+     * - `classes` (string): Space separated string of class names of the element. Those classes can be selected by a style selector.
+     *
+     * The [official Cytoscape.js documentation](http://js.cytoscape.org/#notation/elements-json)
+     * offers an extensive overview and examples of element declaration.
      */
     elements: PropTypes.arrayOf(PropTypes.object),
 
     /**
-     * The Cytoscape stylesheet as non-stringified JSON. N.b. the prop key is
-     * stylesheet rather than style, the key used by Cytoscape itself, so as
-     * to not conflict with the HTML style attribute.
+     * A list of dictionaries representing the styles of the elements. Each
+     * dictionary requires the following keys:
+     * - `selector` (string): Which elements you are styling. Generally, you select a group of elements (node, edges, both), a class (that you declare in the element dictionary), or an element by ID.
+     * - `style` (dictionary): What aspects of the elements you want to modify. This could be the size or color of a node, the shape of an edge arrow, or many more.
+     *
+     * Both [the selector string](http://js.cytoscape.org/#selectors) and
+     * [the style dictionary](http://js.cytoscape.org/#style/node-body) are
+     * exhaustively documented in the Cytoscape.js docs. Although methods such
+     * as `cy.elements(...)` and `cy.filter(...)` are not available, the selector
+     * string syntax stays the same.
      */
     stylesheet: PropTypes.arrayOf(PropTypes.object),
 
     /**
-     * The function of a layout is to set the positions on the nodes in the
-     * graph.
+     * A dictionary specifying how to set the position of the elements in your
+     * graph. * The `'name'` key is required, and indicates which layout (algorithm) to
+     * use. The layouts available by default are:
+     * - `random`: Randomly assigns positions
+     * - `preset`: Assigns position based on the `position` key in element dictionaries
+     * - `circle`: Single-level circle, with optional radius
+     * - `concentric`: Multi-level circle, with optional radius
+     * - `grid`: Square grid, optionally with numbers of `rows` and `cols`
+     * - `breadthfirst`: Tree structure built using BFS, with optional `roots`
+     * - `cose`: Force-directed physics simulation
+     *
+     * The keys accepted by `layout` vary depending on the algorithm, but some
+     * keys are accepted by all layouts:
+     * - `fit` (boolean): Whether to render the nodes in order to fit the canvas.
+     * - `padding` (number): Padding around the sides of the canvas, if fit is enabled.
+     * - `animate` (boolean): Whether to animate change in position when the layout changes.
+     * - `animationDuration` (number): Duration of animation in milliseconds, if enabled.
+     * - `boundingBox` (dictionary): How to constrain the layout in a specific area. Keys accepted are either `x1, y1, x2, y2` or `x1, y1, w, h`, all of which receive a pixel value.
+     * The complete list of layouts and their accepted options are available
+     * on the [Cytoscape.js docs](http://js.cytoscape.org/#layouts).
+     * Note that certain keys are not supported in Dash since the value is a
+     * JavaScript function or a callback. Please visit [this issue](https://github.com/plotly/dash-cytoscape/issues/25)
+     * for more information.
      */
     layout: PropTypes.object,
 
     // Viewport Manipulation
+
     /**
-     * The initial panning position of the graph. Make sure to disable viewport
-     * manipulation options, such as fit, in your layout so that it is not
-     * overridden when the layout is applied.
+     * Dictionary indicating the initial panning position of the graph. The
+     * following keys are accepted:
+     * - `x` (number): The x-coordinate of the position.
+     * - `y` (number): The y-coordinate of the position.
      */
     pan: PropTypes.object,
 
     /**
-     * The initial zoom level of the graph. Make sure to disable viewport
-     * manipulation options, such as fit, in your layout so that it is not
-     * overridden when the layout is applied. You can set options.minZoom and
-     * options.maxZoom to set restrictions on the zoom level.
+     * The initial zoom level of the graph. You can set `minZoom` and
+     * `maxZoom` to set restrictions on the zoom level.
      */
     zoom: PropTypes.number,
 
     // Viewport Mutability and gesture Toggling
     /**
-     * Whether panning the graph is enabled, both by user events and programmatically.
+     * Whether panning the graph is enabled (i.e., the position of the graph is
+     * mutable overall).
      */
     panningEnabled: PropTypes.bool,
 
     /**
-     * Whether user events (e.g. dragging the graph background) are allowed to pan the graph. Programmatic changes to pan are unaffected by this option.
+     * Whether user events (e.g. dragging the graph background) are allowed to
+     * pan the graph.
      */
     userPanningEnabled: PropTypes.bool,
 
     /**
-     * A minimum bound on the zoom level of the graph. The viewport can not be scaled smaller than this zoom level.
+     * A minimum bound on the zoom level of the graph. The viewport can not be
+     * scaled smaller than this zoom level.
      */
     minZoom: PropTypes.number,
 
@@ -405,77 +466,135 @@ Cytoscape.propTypes = {
     maxZoom: PropTypes.number,
 
     /**
-     * Whether zooming the graph is enabled, both by user events and programmatically.
+     * Whether zooming the graph is enabled (i.e., the zoom level of the graph
+     * is mutable overall).
      */
     zoomingEnabled: PropTypes.bool,
 
     /**
-     * Whether user events (e.g. dragging the graph background) are allowed to pan the graph. Programmatic changes to pan are unaffected by this option.
+     * Whether user events (e.g. dragging the graph background) are allowed
+     * to pan the graph.
      */
     userZoomingEnabled: PropTypes.bool,
 
     /**
-     * Whether box selection (i.e. drag a box overlay around, and release it to select) is enabled. If enabled, the user must taphold to pan the graph.
+     * Whether box selection (i.e. drag a box overlay around, and release it
+     * to select) is enabled. If enabled, the user must taphold to pan the graph.
      */
     boxSelectionEnabled: PropTypes.bool,
 
     /**
-     * Whether nodes should be ungrabified (not grabbable by user) by default (if true, overrides individual node state).
+     * Whether nodes should be ungrabified (not grabbable by user) by
+     * default (if true, overrides individual node state).
      */
     autoungrabify: PropTypes.bool,
 
     /**
-     * Whether nodes should be locked (not draggable at all) by default (if true, overrides individual node state).
+     * Whether nodes should be locked (not draggable at all) by default
+     * (if true, overrides individual node state).
      */
     autolock: PropTypes.bool,
 
     /**
-     * Whether nodes should be unselectified (immutable selection state) by default (if true, overrides individual element state).
+     * Whether nodes should be unselectified (immutable selection state) by
+     * default (if true, overrides individual element state).
      */
     autounselectify: PropTypes.bool,
 
     /**
-     * Whether the layout should be refreshed when elements are added or removed
+     * Whether the layout should be refreshed when elements are added or removed.
      */
     autoRefreshLayout: PropTypes.bool,
 
+    // User Events Props
+
     /**
-     * The trimmed node object returned when you tap a node
+     * The complete node dictionary returned when you tap or click it.
+     *
+     * Node-specific items:
+     * - `edgesData` (dictionary)
+     * - `renderedPosition` (dictionary)
+     * - `timeStamp` (number)
+     *
+     * General items (for all elements):
+     * - `classes` (string)
+     * - `data` (dictionary)
+     * - `grabbable` (boolean)
+     * - `group` (string)
+     * - `locked` (boolean)
+     * - `position` (dictionary)
+     * - `selectable` (boolean)
+     * - `selected` (boolean)
+     * - `style` (dictionary)
+     *
+     * Items for compound nodes:
+     * - `ancestorsData` (dictionary)
+     * - `childrenData` (dictionary)
+     * - `descendantsData` (dictionary)
+     * - `parentData` (dictionary)
+     * - `siblingsData` (dictionary)
+     * - `isParent` (boolean)
+     * - `isChildless` (boolean)
+     * - `isChild` (boolean)
+     * - `isOrphan` (boolean)
+     * - `relativePosition` (dictionary)
      */
     tapNode: PropTypes.object,
 
     /**
-     * The data property of the node object returned when you tap a node
+     * The data dictionary of a node returned when you tap or click it.
      */
     tapNodeData: PropTypes.object,
 
     /**
-     * The trimmed edge object returned when you tap a node
+     * The complete edge dictionary returned when you tap or click it.
+     *
+     * Edge-specific items:
+     * - `isLoop` (boolean)
+     * - `isSimple` (boolean)
+     * - `midpoint` (dictionary)
+     * - `sourceData` (dictionary)
+     * - `sourceEndpoint` (dictionary)
+     * - `targetData` (dictionary)
+     * - `targetEndpoint` (dictionary)
+     * - `timeStamp` (number)
+     *
+     * General items (for all elements):
+     * - `classes` (string)
+     * - `data` (dictionary)
+     * - `grabbable` (boolean)
+     * - `group` (string)
+     * - `locked` (boolean)
+     * - `selectable` (boolean)
+     * - `selected` (boolean)
+     * - `style` (dictionary)
      */
     tapEdge: PropTypes.object,
 
     /**
-     * The data property of the edge object returned when you tap a node
+     * The data dictionary of an edge returned when you tap or click it.
      */
     tapEdgeData: PropTypes.object,
 
     /**
-     * The data property of the edge object returned when you hover over a node
+     * The data dictionary of a node returned when you hover over it.
      */
     mouseoverNodeData: PropTypes.object,
 
     /**
-     * The data property of the edge object returned when you hover over an edge
+     * The data dictionary of an edge returned when you hover over it.
      */
     mouseoverEdgeData: PropTypes.object,
 
     /**
-     * The array of node data currently selected by taps and boxes
+     * The list of data dictionaries of all selected nodes (e.g. using
+     * Shift+Click to select multiple nodes, or Shift+Drag to use box selection).
      */
     selectedNodeData: PropTypes.array,
 
     /**
-     * The array of edge data currently selected by taps and boxes
+     * The list of data dictionaries of all selected edges (e.g. using
+     * Shift+Click to select multiple nodes, or Shift+Drag to use box selection).
      */
     selectedEdgeData: PropTypes.array
 };
