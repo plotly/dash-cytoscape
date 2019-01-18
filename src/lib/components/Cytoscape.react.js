6175bbbdf3c2731af779d32f6f79bd935a826697
Xing Han Lu <xhlperso@gmail.com>
2019-01-17 19:56:06 -0500

    Fix Docstring formatting for better markdown generation

--- src/lib/components/Cytoscape.react.js
+++ src/lib/components/Cytoscape.react.js
@@ -359,68 +359,68 @@ Cytoscape.propTypes = {
     // Common props
 
     /**
-     * A list of dictionaries representing the elements of the networks. Each
-     * dictionary describes an element, and specifies its purpose. Keys
-     * accepted are:
-     * - `group` (string): Either 'nodes' or 'edges'. If not given, it's automatically inferred.
-     * - `data` (dictionary): Element specific data.
-     *      - `id` (string): Reference to the element, useful for selectors and edges. Randomly assigned if not given.
-     *      - `label` (string): Optional name for the element, useful when `data(label)` is given to a style's `content` or `label`. It is only a convention.
-     *      - `parent` (string): Only for nodes. Optional reference to another node. Needed to create compound nodes.
-     *      - `source` (string): Only for edges. The id of the source node, which is where the edge starts.
-     *      - `target` (string): Only for edges. The id of the target node, where the edge ends.
-     * - `position` (dictionary): Only for nodes. The position of the node.
-     *      - `x` (number): The x-coordinate of the node.
-     *      - `y` (number): The y-coordinate of the node.
-     * - `selected` (boolean): If the element is selected upon initialisation.
-     * - `selectable` (boolean): If the element can be selected.
-     * - `locked` (boolean): Only for nodes. If the position is immutable.
-     * - `grabbable` (boolean): Only for nodes. If the node can be grabbed and moved by the user.
-     * - `classes` (string): Space separated string of class names of the element. Those classes can be selected by a style selector.
+     * A list of dictionaries representing the elements of the networks.
+     *     1. Each dictionary describes an element, and specifies its purpose.
+     *         - `group` (string): Either 'nodes' or 'edges'. If not given, it's automatically inferred.
+     *         - `data` (dictionary): Element specific data.
+     *              - `id` (string): Reference to the element, useful for selectors and edges. Randomly assigned if not given.
+     *              - `label` (string): Optional name for the element, useful when `data(label)` is given to a style's `content` or `label`. It is only a convention.
+     *              - `parent` (string): Only for nodes. Optional reference to another node. Needed to create compound nodes.
+     *              - `source` (string): Only for edges. The id of the source node, which is where the edge starts.
+     *              - `target` (string): Only for edges. The id of the target node, where the edge ends.
+     *         - `position` (dictionary): Only for nodes. The position of the node.
+     *              - `x` (number): The x-coordinate of the node.
+     *              - `y` (number): The y-coordinate of the node.
+     *         - `selected` (boolean): If the element is selected upon initialisation.
+     *         - `selectable` (boolean): If the element can be selected.
+     *         - `locked` (boolean): Only for nodes. If the position is immutable.
+     *         - `grabbable` (boolean): Only for nodes. If the node can be grabbed and moved by the user.
+     *         - `classes` (string): Space separated string of class names of the element. Those classes can be selected by a style selector.
      *
-     * The [official Cytoscape.js documentation](http://js.cytoscape.org/#notation/elements-json)
-     * offers an extensive overview and examples of element declaration.
+     *     2. The [official Cytoscape.js documentation](http://js.cytoscape.org/#notation/elements-json) offers an extensive overview and examples of element declaration.
      */
     elements: PropTypes.arrayOf(PropTypes.object),
 
     /**
-     * A list of dictionaries representing the styles of the elements. Each
-     * dictionary requires the following keys:
-     * - `selector` (string): Which elements you are styling. Generally, you select a group of elements (node, edges, both), a class (that you declare in the element dictionary), or an element by ID.
-     * - `style` (dictionary): What aspects of the elements you want to modify. This could be the size or color of a node, the shape of an edge arrow, or many more.
+     * A list of dictionaries representing the styles of the elements.
+     *     1. Each dictionary requires the following keys:
+     *         - `selector` (string): Which elements you are styling. Generally, you select a group of elements (node, edges, both), a class (that you declare in the element dictionary), or an element by ID.
+     *         - `style` (dictionary): What aspects of the elements you want to modify. This could be the size or color of a node, the shape of an edge arrow, or many more.
      *
-     * Both [the selector string](http://js.cytoscape.org/#selectors) and
-     * [the style dictionary](http://js.cytoscape.org/#style/node-body) are
-     * exhaustively documented in the Cytoscape.js docs. Although methods such
-     * as `cy.elements(...)` and `cy.filter(...)` are not available, the selector
-     * string syntax stays the same.
+     *     2. Both [the selector string](http://js.cytoscape.org/#selectors) and
+     *     [the style dictionary](http://js.cytoscape.org/#style/node-body) are
+     *     exhaustively documented in the Cytoscape.js docs. Although methods such
+     *     as `cy.elements(...)` and `cy.filter(...)` are not available, the selector
+     *     string syntax stays the same.
      */
     stylesheet: PropTypes.arrayOf(PropTypes.object),
 
     /**
      * A dictionary specifying how to set the position of the elements in your
-     * graph. * The `'name'` key is required, and indicates which layout (algorithm) to
-     * use. The layouts available by default are:
-     * - `random`: Randomly assigns positions
-     * - `preset`: Assigns position based on the `position` key in element dictionaries
-     * - `circle`: Single-level circle, with optional radius
-     * - `concentric`: Multi-level circle, with optional radius
-     * - `grid`: Square grid, optionally with numbers of `rows` and `cols`
-     * - `breadthfirst`: Tree structure built using BFS, with optional `roots`
-     * - `cose`: Force-directed physics simulation
+     * graph. The `'name'` key is required, and indicates which layout (algorithm) to
+     * use.
+     *     1. The layouts available by default are:
+     *         - `random`: Randomly assigns positions
+     *         - `preset`: Assigns position based on the `position` key in element dictionaries
+     *         - `circle`: Single-level circle, with optional radius
+     *         - `concentric`: Multi-level circle, with optional radius
+     *         - `grid`: Square grid, optionally with numbers of `rows` and `cols`
+     *         - `breadthfirst`: Tree structure built using BFS, with optional `roots`
+     *         - `cose`: Force-directed physics simulation
      *
-     * The keys accepted by `layout` vary depending on the algorithm, but some
-     * keys are accepted by all layouts:
-     * - `fit` (boolean): Whether to render the nodes in order to fit the canvas.
-     * - `padding` (number): Padding around the sides of the canvas, if fit is enabled.
-     * - `animate` (boolean): Whether to animate change in position when the layout changes.
-     * - `animationDuration` (number): Duration of animation in milliseconds, if enabled.
-     * - `boundingBox` (dictionary): How to constrain the layout in a specific area. Keys accepted are either `x1, y1, x2, y2` or `x1, y1, w, h`, all of which receive a pixel value.
-     * The complete list of layouts and their accepted options are available
-     * on the [Cytoscape.js docs](http://js.cytoscape.org/#layouts).
-     * Note that certain keys are not supported in Dash since the value is a
-     * JavaScript function or a callback. Please visit [this issue](https://github.com/plotly/dash-cytoscape/issues/25)
-     * for more information.
+     *     2. The keys accepted by `layout` vary depending on the algorithm, but some
+     *     keys are accepted by all layouts:
+     *         - `fit` (boolean): Whether to render the nodes in order to fit the canvas.
+     *         - `padding` (number): Padding around the sides of the canvas, if fit is enabled.
+     *         - `animate` (boolean): Whether to animate change in position when the layout changes.
+     *         - `animationDuration` (number): Duration of animation in milliseconds, if enabled.
+     *         - `boundingBox` (dictionary): How to constrain the layout in a specific area. Keys accepted are either `x1, y1, x2, y2` or `x1, y1, w, h`, all of which receive a pixel value.
+     *
+     *     3. The complete list of layouts and their accepted options are available
+     *     on the [Cytoscape.js docs](http://js.cytoscape.org/#layouts).
+     *     Note that certain keys are not supported in Dash since the value is a
+     *     JavaScript function or a callback. Please visit [this issue](https://github.com/plotly/dash-cytoscape/issues/25)
+     *     for more information.
      */
     layout: PropTypes.object,
 
@@ -429,8 +429,8 @@ Cytoscape.propTypes = {
     /**
      * Dictionary indicating the initial panning position of the graph. The
      * following keys are accepted:
-     * - `x` (number): The x-coordinate of the position.
-     * - `y` (number): The y-coordinate of the position.
+     *     - `x` (number): The x-coordinate of the position.
+     *     - `y` (number): The y-coordinate of the position.
      */
     pan: PropTypes.object,
 
@@ -511,33 +511,33 @@ Cytoscape.propTypes = {
     /**
      * The complete node dictionary returned when you tap or click it.
      *
-     * Node-specific items:
-     * - `edgesData` (dictionary)
-     * - `renderedPosition` (dictionary)
-     * - `timeStamp` (number)
+     *     1. Node-specific items:
+     *         - `edgesData` (dictionary)
+     *         - `renderedPosition` (dictionary)
+     *         - `timeStamp` (number)
      *
-     * General items (for all elements):
-     * - `classes` (string)
-     * - `data` (dictionary)
-     * - `grabbable` (boolean)
-     * - `group` (string)
-     * - `locked` (boolean)
-     * - `position` (dictionary)
-     * - `selectable` (boolean)
-     * - `selected` (boolean)
-     * - `style` (dictionary)
+     *     2. General items (for all elements):
+     *         - `classes` (string)
+     *         - `data` (dictionary)
+     *         - `grabbable` (boolean)
+     *         - `group` (string)
+     *         - `locked` (boolean)
+     *         - `position` (dictionary)
+     *         - `selectable` (boolean)
+     *         - `selected` (boolean)
+     *         - `style` (dictionary)
      *
-     * Items for compound nodes:
-     * - `ancestorsData` (dictionary)
-     * - `childrenData` (dictionary)
-     * - `descendantsData` (dictionary)
-     * - `parentData` (dictionary)
-     * - `siblingsData` (dictionary)
-     * - `isParent` (boolean)
-     * - `isChildless` (boolean)
-     * - `isChild` (boolean)
-     * - `isOrphan` (boolean)
-     * - `relativePosition` (dictionary)
+     *     3. Items for compound nodes:
+     *         - `ancestorsData` (dictionary)
+     *         - `childrenData` (dictionary)
+     *         - `descendantsData` (dictionary)
+     *         - `parentData` (dictionary)
+     *         - `siblingsData` (dictionary)
+     *         - `isParent` (boolean)
+     *         - `isChildless` (boolean)
+     *         - `isChild` (boolean)
+     *         - `isOrphan` (boolean)
+     *         - `relativePosition` (dictionary)
      */
     tapNode: PropTypes.object,
 
@@ -549,25 +549,25 @@ Cytoscape.propTypes = {
     /**
      * The complete edge dictionary returned when you tap or click it.
      *
-     * Edge-specific items:
-     * - `isLoop` (boolean)
-     * - `isSimple` (boolean)
-     * - `midpoint` (dictionary)
-     * - `sourceData` (dictionary)
-     * - `sourceEndpoint` (dictionary)
-     * - `targetData` (dictionary)
-     * - `targetEndpoint` (dictionary)
-     * - `timeStamp` (number)
+     *     1. Edge-specific items:
+     *         - `isLoop` (boolean)
+     *         - `isSimple` (boolean)
+     *         - `midpoint` (dictionary)
+     *         - `sourceData` (dictionary)
+     *         - `sourceEndpoint` (dictionary)
+     *         - `targetData` (dictionary)
+     *         - `targetEndpoint` (dictionary)
+     *         - `timeStamp` (number)
      *
-     * General items (for all elements):
-     * - `classes` (string)
-     * - `data` (dictionary)
-     * - `grabbable` (boolean)
-     * - `group` (string)
-     * - `locked` (boolean)
-     * - `selectable` (boolean)
-     * - `selected` (boolean)
-     * - `style` (dictionary)
+     *     2. General items (for all elements):
+     *         - `classes` (string)
+     *         - `data` (dictionary)
+     *         - `grabbable` (boolean)
+     *         - `group` (string)
+     *         - `locked` (boolean)
+     *         - `selectable` (boolean)
+     *         - `selected` (boolean)
+     *         - `style` (dictionary)
      */
     tapEdge: PropTypes.object,
 
