e9f9d013932fb103865d727f96d48a451707c573
Xing Han Lu <xhlperso@gmail.com>
2019-02-27 23:22:46 -0500

    :snowflake: Flake 8 on usage.py

--- usage.py
+++ usage.py
@@ -9,9 +9,11 @@ app.layout = html.Div([
     cyto.Cytoscape(
         id='cytoscape',
         elements=[
-            {'data': {'id': 'one', 'label': 'Node 1'}, 'position': {'x': 50, 'y': 50}},
-            {'data': {'id': 'two', 'label': 'Node 2'}, 'position': {'x': 200, 'y': 200}},
-            {'data': {'source': 'one', 'target': 'two','label': 'Node 1 to 2'}}
+            {'data': {'id': 'one', 'label': 'Node 1'},
+             'position': {'x': 50, 'y': 50}},
+            {'data': {'id': 'two', 'label': 'Node 2'},
+             'position': {'x': 200, 'y': 200}},
+            {'data': {'source': 'one', 'target': 'two', 'label': '1 to 2'}}
         ],
         layout={'name': 'preset'}
     )
