504d17d95c8e9ec77aba20db01cdc8db2acaeb1d
Xing Han Lu <xhlperso@gmail.com>
2019-03-30 21:41:30 -0400

    :new: Create extra_index.js to load external layouts
    Update Cytoscape.react.js description as well

---
+++ src/lib/extra_index.js
@@ -0,0 +1,20 @@
+/* eslint-disable import/prefer-default-export */
+import Cytoscape from './components/Cytoscape.react';
+import CytoscapeJS from 'cytoscape';
+import coseBilkent from 'cytoscape-cose-bilkent';
+import cola from 'cytoscape-cola';
+import dagre from 'cytoscape-dagre';
+import euler from 'cytoscape-euler';
+import klay from 'cytoscape-klay';
+import spread from 'cytoscape-spread';
+
+CytoscapeJS.use(coseBilkent);
+CytoscapeJS.use(cola);
+CytoscapeJS.use(dagre);
+CytoscapeJS.use(euler);
+CytoscapeJS.use(klay);
+CytoscapeJS.use(spread);
+
+export {
+    Cytoscape
+};
