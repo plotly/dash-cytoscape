90bfe1886eddcc009508a72935dbae7a1d223b5f
Ryan Patrick Kyle <ryan@plot.ly>
2019-06-25 13:54:53 -0400

    :package: updated for compat w/ dash 0.1.0

--- R/internal.R
+++ R/internal.R
@@ -2,14 +2,25 @@
 deps_metadata <- list(`dash_cytoscape` = structure(list(name = "dash_cytoscape",
 version = "0.1.1", src = list(href = NULL,
 file = "deps"), meta = NULL,
-script = "dash_cytoscape.min.js",
-stylesheet = NULL, head = NULL, attachment = NULL, package = "dashCytoscape",
-all_files = FALSE), class = "html_dependency"),
-`dash_cytoscape_dev` = structure(list(name = "dash_cytoscape",
-version = "0.1.1", src = list(href = NULL,
-file = "deps"), meta = NULL,
-script = "dash_cytoscape.dev.js",
+script = 'dash_cytoscape.min.js',
 stylesheet = NULL, head = NULL, attachment = NULL, package = "dashCytoscape",
 all_files = FALSE), class = "html_dependency"))
 return(deps_metadata)
+}
+
+dash_assert_valid_wildcards <- function (attrib = list("data", "aria"), ...)
+{
+    args <- list(...)
+    validation_results <- lapply(names(args), function(x) {
+        grepl(paste0("^", attrib, "-[a-zA-Z0-9]{1,}$", collapse = "|"),
+            x)
+    })
+    if (FALSE %in% validation_results) {
+        stop(sprintf("The following wildcards are not currently valid in Dash: '%s'",
+            paste(names(args)[grepl(FALSE, unlist(validation_results))],
+                collapse = ", ")), call. = FALSE)
+    }
+    else {
+        return(args)
+    }
 }
