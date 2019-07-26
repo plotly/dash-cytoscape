184fc904cd2924e44d501c21c393f92284c0f3a1
Manfred Cheung <mj3cheun@uwaterloo.ca>
2019-07-26 18:05:05 -0400

    Improve function and variable names

--- src/lib/components/Resize.js
+++ src/lib/components/Resize.js
@@ -3,7 +3,7 @@ export default class Resize {
         this.shouldResize = false;
 
         this.cy = cy;
-        this.cur = {};
+        this.curr = {};
         this.prev = {};
 
         this.marginPercentage = {};
@@ -14,10 +14,10 @@ export default class Resize {
         this.toggle = this.toggle.bind(this);
         this.getViewport = this.getViewport.bind(this);
         this.updateViewport = this.updateViewport.bind(this);
-        this.xConstrainedZoom = this.xConstrainedZoom.bind(this);
-        this.xChangeMargin = this.xChangeMargin.bind(this);
-        this.yConstrainedZoom = this.yConstrainedZoom.bind(this);
-        this.yChangeMargin = this.yChangeMargin.bind(this);
+        this._xConstrainedZoom = this._xConstrainedZoom.bind(this);
+        this._xChangeMargin = this._xChangeMargin.bind(this);
+        this._yConstrainedZoom = this._yConstrainedZoom.bind(this);
+        this._yChangeMargin = this._yChangeMargin.bind(this);
         this.resize = this.resize.bind(this);
     }
 
@@ -60,31 +60,31 @@ export default class Resize {
 
     /////////////////////////////////////////////////////////////////
 
-    xConstrainedZoom(level) {
-        const {cur, prev, marginPercentage} = this;
+    _xConstrainedZoom(level) {
+        const {curr, prev, marginPercentage} = this;
 
     // Look at constrained dimension
         // calculate the new position needed to maintain same left/right margin percentages
         // with new zoom
-        const newRenderedBBX1 = marginPercentage.left * cur.width;
+        const newRenderedBBX1 = marginPercentage.left * curr.width;
 
-        cur.position.x = newRenderedBBX1 + (prev.position.x - prev.renderedBB.x1);
+        curr.position.x = newRenderedBBX1 + (prev.position.x - prev.renderedBB.x1);
 
     // Look at unconstrained dimension
         // get midpoint
-        const midpoint = cur.renderedBB.y1 + cur.renderedBB.h / 2;
+        const midpoint = curr.renderedBB.y1 + curr.renderedBB.h / 2;
         // get new height of graph
-        const newHeight = cur.renderedBB.h / prev.zoom * level;
+        const newHeight = curr.renderedBB.h / prev.zoom * level;
         // get new top margin
         let newRenderedBBY1 = midpoint - (newHeight / 2);
-        // compensate for any change in height between prev and cur
-        newRenderedBBY1 = newRenderedBBY1 + (cur.height - prev.height) / 2;
+        // compensate for any change in height between prev and curr
+        newRenderedBBY1 = newRenderedBBY1 + (curr.height - prev.height) / 2;
 
-        cur.position.y = newRenderedBBY1 + (prev.position.y - prev.renderedBB.y1);
+        curr.position.y = newRenderedBBY1 + (prev.position.y - prev.renderedBB.y1);
     }
 
-    xChangeMargin(width) {
-        const {cur, prev} = this;
+    _xChangeMargin(width) {
+        const {curr, prev} = this;
 
         // get initial centroid percentage from the BB left boundary
         const xCentroidPos = prev.renderedBB.x1 + prev.renderedBB.w / 2;
@@ -94,43 +94,43 @@ export default class Resize {
         const newXCentroidPos = xCentroidPosPer * width;
 
         // Find displacement in x required to bring to new position
-        cur.position.x = cur.position.x + (newXCentroidPos - xCentroidPos);
+        curr.position.x = curr.position.x + (newXCentroidPos - xCentroidPos);
     }
 
-    yConstrainedZoom(level) {
-        // same as this.xConstrainedZoom except x and y switched where applicable
-        const {cur, prev, marginPercentage} = this;
+    _yConstrainedZoom(level) {
+        // same as this._xConstrainedZoom except x and y switched where applicable
+        const {curr, prev, marginPercentage} = this;
 
-        const newRenderedBBY1 = marginPercentage.top * cur.height;
-        cur.position.y = newRenderedBBY1 + (prev.position.y - prev.renderedBB.y1);
+        const newRenderedBBY1 = marginPercentage.top * curr.height;
+        curr.position.y = newRenderedBBY1 + (prev.position.y - prev.renderedBB.y1);
 
-        const midpoint = cur.renderedBB.x1 + cur.renderedBB.w / 2;
-        const newWidth = cur.renderedBB.w / prev.zoom * level;
+        const midpoint = curr.renderedBB.x1 + curr.renderedBB.w / 2;
+        const newWidth = curr.renderedBB.w / prev.zoom * level;
         let newRenderedBBX1 = midpoint - (newWidth / 2);
-        newRenderedBBX1 = newRenderedBBX1 + (cur.width - prev.width) / 2;
+        newRenderedBBX1 = newRenderedBBX1 + (curr.width - prev.width) / 2;
 
-        cur.position.x = newRenderedBBX1 + (prev.position.x - prev.renderedBB.x1);
+        curr.position.x = newRenderedBBX1 + (prev.position.x - prev.renderedBB.x1);
     }
 
-    yChangeMargin() {
-        // same as xChangeMargin except x and y switched where applicable
-        const {cur, prev} = this;
+    _yChangeMargin() {
+        // same as _xChangeMargin except x and y switched where applicable
+        const {curr, prev} = this;
 
         const yCentroidPos = prev.renderedBB.y1 + prev.renderedBB.h / 2;
         const yCentroidPosPer = yCentroidPos / prev.height;
 
-        const newYCentroidPos = yCentroidPosPer * cur.height;
+        const newYCentroidPos = yCentroidPosPer * curr.height;
 
-        cur.position.y = cur.position.y + (newYCentroidPos - yCentroidPos);
+        curr.position.y = curr.position.y + (newYCentroidPos - yCentroidPos);
     }
 
     /////////////////////////////////////////////////////////////////
 
     resize() {
         const cy = this.cy;
-        this.cur = this.getViewport(cy);
+        this.curr = this.getViewport(cy);
 
-        const {cur, prev} = this;
+        const {curr, prev} = this;
 
         // Is the figure fully contained in the viewport?
         const fullyContained = (prev.renderedBB.x1 >= 0)
@@ -147,106 +147,106 @@ export default class Resize {
         };
 
         // Find constrained dimension
-        // Find which dimension has changed the most as a percentage of the current dimensions
-        const xConstrained = Math.abs(1 - (cur.width / prev.width))
-            > Math.abs(1 - (cur.height / prev.height));
+        // Find which dimension has changed the most as a percentage of the currrent dimensions
+        const xConstrained = Math.abs(1 - (curr.width / prev.width))
+            > Math.abs(1 - (curr.height / prev.height));
 
         // define output variables
         if(xConstrained) {
             // calculate zoom so that the width remains same
-            const targetZoom = prev.zoom / prev.width * cur.width;
+            const targetZoom = prev.zoom / prev.width * curr.width;
             if(fullyContained) {
                 const maxContainedZoom = Math.min(
-                    (cur.renderedBB.y1 + (cur.renderedBB.h / 2)) * prev.zoom * 2 / cur.renderedBB.h,
-                    - (cur.renderedBB.y1 + (cur.renderedBB.h / 2) - prev.height) * prev.zoom * 2 / cur.renderedBB.h
+                    (curr.renderedBB.y1 + (curr.renderedBB.h / 2)) * prev.zoom * 2 / curr.renderedBB.h,
+                    - (curr.renderedBB.y1 + (curr.renderedBB.h / 2) - prev.height) * prev.zoom * 2 / curr.renderedBB.h
                 ) - this.containedZoomMargin;
                 const maxContainedWidth = prev.width / prev.zoom * maxContainedZoom;
 
                 // Setup state machine and required variables
-                let currentState = cur.zoom < maxContainedZoom ? -1 : 1;
+                let currrentState = curr.zoom < maxContainedZoom ? -1 : 1;
                 const targetState = targetZoom < maxContainedZoom ? -1 : 1;
-                while(Math.abs(currentState) <= 1) {
+                while(Math.abs(currrentState) <= 1) {
                     // set an intermediate target zoom if a transition through the maxContained level is needed
-                    if(currentState === -1) {
+                    if(currrentState === -1) {
                         const intermediateTargetZoom = targetState === 1
                             ? maxContainedZoom
                             : targetZoom;
-                        this.xConstrainedZoom(intermediateTargetZoom);
-                        cur.zoom = intermediateTargetZoom;
+                        this._xConstrainedZoom(intermediateTargetZoom);
+                        curr.zoom = intermediateTargetZoom;
                         if(targetState === 1) {
-                            this.prev.position = this.cur.position;
+                            this.prev.position = this.curr.position;
                             this.prev.width = maxContainedWidth;
                         }
                     }
-                    else { // if(currentState === 1) {
+                    else { // if(currrentState === 1) {
                         const intermediateTargetWidth = targetState === -1
                             ? maxContainedWidth
-                            : cur.width;
-                        this.xChangeMargin(intermediateTargetWidth);
+                            : curr.width;
+                        this._xChangeMargin(intermediateTargetWidth);
                         if(targetState === -1) {
-                            this.prev.position = this.cur.position;
+                            this.prev.position = this.curr.position;
                             this.prev.width = maxContainedWidth;
                         }
                     }
 
-                    currentState += 2 * targetState;
+                    currrentState += 2 * targetState;
                 }
             }
             else {
-                cur.zoom = targetZoom;
-                this.xConstrainedZoom(cur.zoom);
+                curr.zoom = targetZoom;
+                this._xConstrainedZoom(curr.zoom);
             }
         }
         else { // is y Constrained
             // calculate zoom so that the width remains same
-            const targetZoom = prev.zoom / prev.height * cur.height;
+            const targetZoom = prev.zoom / prev.height * curr.height;
             if(fullyContained) {
                 const maxContainedZoom = Math.min(
-                    (cur.renderedBB.x1 + (cur.renderedBB.w / 2)) * prev.zoom * 2 / cur.renderedBB.w,
-                    - (cur.renderedBB.x1 + (cur.renderedBB.w / 2) - prev.width) * prev.zoom * 2 / cur.renderedBB.w
+                    (curr.renderedBB.x1 + (curr.renderedBB.w / 2)) * prev.zoom * 2 / curr.renderedBB.w,
+                    - (curr.renderedBB.x1 + (curr.renderedBB.w / 2) - prev.width) * prev.zoom * 2 / curr.renderedBB.w
                 ) - this.containedZoomMargin;
                 const maxContainedHeight = prev.height / prev.zoom * maxContainedZoom;
 
                 // Setup state machine and required variables
-                let currentState = cur.zoom < maxContainedZoom ? -1 : 1;
+                let currrentState = curr.zoom < maxContainedZoom ? -1 : 1;
                 const targetState = targetZoom < maxContainedZoom ? -1 : 1;
-                while(Math.abs(currentState) <= 1) {
+                while(Math.abs(currrentState) <= 1) {
                     // set an intermediate target zoom if a transition through the maxContained level is needed
-                    if(currentState === -1) {
+                    if(currrentState === -1) {
                         const intermediateTargetZoom = targetState === 1
                             ? maxContainedZoom
                             : targetZoom;
-                        this.yConstrainedZoom(intermediateTargetZoom);
-                        cur.zoom = intermediateTargetZoom;
+                        this._yConstrainedZoom(intermediateTargetZoom);
+                        curr.zoom = intermediateTargetZoom;
                         if(targetState === 1) {
-                            this.prev.position = this.cur.position;
+                            this.prev.position = this.curr.position;
                             this.prev.height = maxContainedHeight;
                         }
                     }
-                    else { // if(currentState === 1) {
+                    else { // if(currrentState === 1) {
                         const intermediateTargetHeight = targetState === -1
                             ? maxContainedHeight
-                            : cur.height;
-                        this.yChangeMargin(intermediateTargetHeight);
+                            : curr.height;
+                        this._yChangeMargin(intermediateTargetHeight);
                         if(targetState === -1) {
-                            this.prev.position = this.cur.position;
+                            this.prev.position = this.curr.position;
                             this.prev.height = maxContainedHeight;
                         }
                     }
 
-                    currentState += 2 * targetState;
+                    currrentState += 2 * targetState;
                 }
             }
             else {
-                cur.zoom = targetZoom;
-                this.yConstrainedZoom(cur.zoom);
+                curr.zoom = targetZoom;
+                this._yConstrainedZoom(curr.zoom);
             }
         }
 
-        cy.zoom({level: cur.zoom});
-        cy.pan(cur.position);
-        this.prev = this.cur;
+        cy.zoom({level: curr.zoom});
+        cy.pan(curr.position);
+        this.prev = this.curr;
 
-        return cur;
+        return curr;
     }
 };
