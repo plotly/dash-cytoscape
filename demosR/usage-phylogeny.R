38c381a9c7928279e6b6f98debb60bfd48a9729d
ohaanika <aanika.rahman@mail.mcgill.ca>
2019-08-11 08:09:18 -0400

    update usage-phylogeny.R

--- demosR/usage-phylogeny.R
+++ demosR/usage-phylogeny.R
@@ -8,7 +8,9 @@ app <- Dash$new()
 tree <- xmlToList(xmlParse(file = "demos/data/apaf.xml"))$phylogeny
 capture.output(tree, file = "demosR/tree.txt", append = FALSE)
 
-generate_elements <- function(tree, xlen=30, ylen=30, grabbable=False) {
+generate_elements <- function(tree, xlen=30, ylen=30, grabbable=FALSE,
+                              nodes=list(), edges=list(), i=list(), n=1) {
+
   # there are 31 "terminal" nodes with their respective 31 "support" nodes
   # and 30 "nonterminal" nodes with their respective 29 "support" nodes
   # since the root node does not require a preceding "support" node;
@@ -17,117 +19,115 @@ generate_elements <- function(tree, xlen=30, ylen=30, grabbable=False) {
   # the 90 degree angle between the parent and the children;
   # edge config: parent -> support -> child
 
-  organize_tree <- function(tree, nodes=list(), edges=list(), i=list(), n=1) {
-    # append root element
-    nodes[[n]] <- list(
-      data = list(id = "r"),
-      position = list(x = 30, y = 1200),
-      classes = "nonterminal",
-      grabbable = FALSE
-    )
-    # function to append all nodes and respective support nodes
-    explore_child <- function(child, child_id) {
-      if (class(child) == "list") {
-        if (!is.null(child$name)) {
+  # append root element
+  nodes[[n]] <- list(
+    data = list(id = "r"),
+    position = list(x = 30, y = 1200),
+    classes = "nonterminal",
+    grabbable = FALSE
+  )
+  # function to append all nodes and respective support nodes
+  explore_child <- function(child, child_id) {
+    if (class(child) == "list") {
+      if (!is.null(child$name)) {
+        support_id <- paste0(child_id, "s", i[[length(i)]])
+        child_id <- paste0(child_id, "c", i[[length(i)]])
+        n <<- n+2
+        nodes[[n-1]] <<- list(
+          data = list(id = support_id),
+          # TODO: edit (x,y) according to branch_length = child$branch_length
+          # position = list(x = 0, y = 0),
+          classes = "support",
+          grabbable = FALSE
+        )
+        nodes[[n]] <<- list(
+          data = list(id = child_id, name = child$name),
+          # TODO: edit (x,y) according to branch_length = child$branch_length
+          # position = list(x = 0, y = 0),
+          classes = "terminal",
+          grabbable = FALSE
+        )
+        i[[length(i)]] <<- i[[length(i)]]+1
+      } else {
+        if (!is.null(child$confidence$text)) {
           support_id <- paste0(child_id, "s", i[[length(i)]])
           child_id <- paste0(child_id, "c", i[[length(i)]])
           n <<- n+2
           nodes[[n-1]] <<- list(
             data = list(id = support_id),
             # TODO: edit (x,y) according to branch_length = child$branch_length
-            position = list(x = 0, y = 0),
+            # position = list(x = 0, y = 0),
             classes = "support",
             grabbable = FALSE
           )
           nodes[[n]] <<- list(
-            data = list(id = child_id, name = child$name),
+            data = list(id = child_id, confidence = child$confidence$text),
             # TODO: edit (x,y) according to branch_length = child$branch_length
-            position = list(x = 0, y = 0),
-            classes = "terminal",
+            # position = list(x = 0, y = 0),
+            classes = "nonterminal",
             grabbable = FALSE
           )
           i[[length(i)]] <<- i[[length(i)]]+1
-        } else {
-          if (!is.null(child$confidence$text)) {
-            support_id <- paste0(child_id, "s", i[[length(i)]])
-            child_id <- paste0(child_id, "c", i[[length(i)]])
-            n <<- n+2
-            nodes[[n-1]] <<- list(
-              data = list(id = support_id),
-              # TODO: edit (x,y) according to branch_length = child$branch_length
-              position = list(x = 0, y = 0),
-              classes = "support",
-              grabbable = FALSE
-            )
-            nodes[[n]] <<- list(
-              data = list(id = child_id, confidence = child$confidence$text),
-              # TODO: edit (x,y) according to branch_length = child$branch_length
-              position = list(x = 0, y = 0),
-              classes = "nonterminal",
-              grabbable = FALSE
-            )
-            i[[length(i)]] <<- i[[length(i)]]+1
-          }
-          i[[length(i)+1]] <<- 0
-          lapply(child, explore_child, child_id=child_id)
-          i <<- i[-length(i)]
         }
+        i[[length(i)+1]] <<- 0
+        lapply(child, explore_child, child_id=child_id)
+        i <<- i[-length(i)]
       }
     }
-    lapply(tree, explore_child, child_id="r")
-
-    str(nodes)
-    print(paste("SIZE", length(nodes)))
-
-    return(nodes)
   }
+  lapply(tree, explore_child, child_id="r")
 
-  get_terminals <- function(tree, terminals=list()) {
-    get_name <- function(child) {
-      if (class(child) == "list") {
-        if (!is.null(child$name)) {
-          terminals[[length(terminals)+1]] <<- list(
-            name = child$name,
-            branch_length = child$branch_length
-          )
-        } else {
-          lapply(child, get_name)
-        }
-      }
-    }
-    lapply(tree, get_name)
-    return(terminals)
-  }
-
-  get_col_positions <- function(tree, taxa, column_width=80) {
-    # note constants for drawing calculations
-    max_label_width <- max(unlist(lapply(taxa, function(taxon) return(nchar(taxon$name)))))
-    drawing_width <- column_width - max_label_width - 1
-
-    return(list())
-  }
-
-  get_row_positions <- function(tree, taxa) {
-    positions <- lapply(1:length(taxa), function(id) return(2*(id-1)))
-    names(positions) <- lapply(taxa, function(taxon) return(taxon$name))
-
-    return(list())
-  }
-
-  clean_tree <- organize_tree(tree)
-  taxa <- get_terminals(tree)
-  col_positions <- get_col_positions(tree, taxa)
-  row_positions <- get_row_positions(tree, taxa)
-
-  nodes <- list()
-  edges <- list()
+  print("NODES")
+  str(nodes)
+  print(paste("SIZE", length(nodes)))
+  print("EDGES")
+  str(edges)
+  print(paste("SIZE", length(edges)))
 
   # TODO: remove dummy data to not overwrite outputs using functions above
   nodes <- list(1,2,3)
   edges <- list(4,5,6)
-  elements <- c(nodes, edges)
 
-  return (elements)
+  elements <- c(nodes, edges)
+  return(elements)
+
+  # TODO: remove these drafts
+  # get_terminals <- function(tree, terminals=list()) {
+  #   get_name <- function(child) {
+  #     if (class(child) == "list") {
+  #       if (!is.null(child$name)) {
+  #         terminals[[length(terminals)+1]] <<- list(
+  #           name = child$name,
+  #           branch_length = child$branch_length
+  #         )
+  #       } else {
+  #         lapply(child, get_name)
+  #       }
+  #     }
+  #   }
+  #   lapply(tree, get_name)
+  #   return(terminals)
+  # }
+  #
+  # get_col_positions <- function(tree, taxa, column_width=80) {
+  #   # note constants for drawing calculations
+  #   max_label_width <- max(unlist(lapply(taxa, function(taxon) return(nchar(taxon$name)))))
+  #   drawing_width <- column_width - max_label_width - 1
+  #
+  #   return(list())
+  # }
+  #
+  # get_row_positions <- function(tree, taxa) {
+  #   positions <- lapply(1:length(taxa), function(id) return(2*(id-1)))
+  #   names(positions) <- lapply(taxa, function(taxon) return(taxon$name))
+  #
+  #   return(list())
+  # }
+  #
+  # taxa <- get_terminals(tree)
+  # col_positions <- get_col_positions(tree, taxa)
+  # row_positions <- get_row_positions(tree, taxa)
+
 }
 
 elements <- generate_elements(tree)
