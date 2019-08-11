81b67fad6a550a226c6e9c96c4c989483ab3d159
ohaanika <aanika.rahman@mail.mcgill.ca>
2019-08-11 07:09:50 -0400

    update usage-phylogeny.R

--- demosR/usage-phylogeny.R
+++ demosR/usage-phylogeny.R
@@ -5,12 +5,168 @@ library(XML)
 
 app <- Dash$new()
 
-result <- xmlParse(file = "demos/data/apaf.xml")
-# print(typeof(result))
+tree <- xmlToList(xmlParse(file = "demos/data/apaf.xml"))$phylogeny
+capture.output(tree, file = "demosR/tree.txt", append = FALSE)
 
-tree <- xmlToList(result)
-# print(typeof(tree))
-str(tree)
+# nodes: 31 terminal, 31 support; 30 nonterminal, 29 support (root has no support)
+
+generate_elements <- function(tree, xlen=30, ylen=30, grabbable=False) {
+  organize_tree <- function(tree, terminals=list(), nonterminals=list(), i=list(), t=0, n=0) {
+
+    explore_child <- function(child, id) {
+      if (class(child) == "list") {
+        if (!is.null(child$name)) {
+          t <<- t+1
+          id <- paste0(id, "c", i[[length(i)]])
+          i[[length(i)]] <<- i[[length(i)]]+1
+          terminals[[t]] <<- list(
+            id = id,
+            tmpID = paste0("t-", t),
+            name = child$name,
+            branch_length = child$branch_length,
+            classes = "terminal"
+          )
+        } else {
+          if (!is.null(child$confidence$text)) {
+            n <<- n+1
+            id <- paste0(id, "c", i[[length(i)]])
+            i[[length(i)]] <<- i[[length(i)]]+1
+            nonterminals[[n]] <<- list(
+              id = id,
+              tmpID = paste0("n-", n),
+              confidence = child$confidence$text,
+              branch_length = child$branch_length,
+              classes = "nonterminal"
+            )
+          }
+          i[[length(i)+1]] <<- 0
+          lapply(child, explore_child, id=id)
+          i <<- i[-length(i)]
+        }
+      }
+    }
+    lapply(tree, explore_child, id="r")
+
+    # CORRECT SO FAR
+    # explore_child <- function(child, id) {
+    #   if (class(child) == "list") {
+    #     #i <<- i+1
+    #     #id <- paste0(id, "c", i)
+    #     if (!is.null(child$name)) {
+    #       t <<- t+1
+    #       terminals[[t]] <<- list(
+    #         #id = id,
+    #         tmpID = paste0("t-", t),
+    #         name = child$name,
+    #         branch_length = child$branch_length,
+    #         classes = "terminal"
+    #       )
+    #     } else {
+    #       if (!is.null(child$confidence$text)) {
+    #         n <<- n+1
+    #         nonterminals[[n]] <<- list(
+    #           #id = id,
+    #           tmpID = paste0("n-", n),
+    #           confidence = child$confidence$text,
+    #           branch_length = child$branch_length,
+    #           classes = "nonterminal"
+    #         )
+    #       }
+    #       lapply(child, explore_child, id=id)
+    #     }
+    #     #i <<- i-1
+    #   }
+    # }
+    # lapply(tree, explore_child, id="r")
+
+    # FAILED ATTEMPT
+    # explore_child <- function(child) {
+    #   if (class(child) == "list") {
+    #     if (!is.null(child$name)) {
+    #       return(
+    #         list(
+    #           id = paste("t-", child$name),
+    #           name = child$name,
+    #           branch_length = child$branch_length,
+    #           classes = "terminal"
+    #         )
+    #       )
+    #     } else if (!is.null(child$confidence$text)) {
+    #       return(
+    #         list(
+    #           id = paste("n-", child$confidence$text),
+    #           confidence = child$confidence$text,
+    #           branch_length = child$branch_length,
+    #           classes = "nonterminal",
+    #           child = lapply(child, explore_child)
+    #         )
+    #       )
+    #     } else {
+    #       return(lapply(child, explore_child))
+    #     }
+    #   }
+    # }
+    # nodes <- lapply(tree, explore_child)
+
+    # print("TERMINALS")
+    # str(terminals)
+    # print(paste("SIZE", length(terminals)))
+    # print("NONTERMINALS")
+    # str(nonterminals)
+    # print(paste("SIZE", length(nonterminals)))
+
+    return(list(terminals = terminals, nonterminals = nonterminals))
+  }
+
+  get_terminals <- function(tree, terminals=list()) {
+    get_name <- function(child) {
+      if (class(child) == "list") {
+        if (!is.null(child$name)) {
+          terminals[[length(terminals)+1]] <<- list(
+            name = child$name,
+            branch_length = child$branch_length
+          )
+        } else {
+          lapply(child, get_name)
+        }
+      }
+    }
+    lapply(tree, get_name)
+    return(terminals)
+  }
+
+  get_col_positions <- function(tree, taxa, column_width=80) {
+    # note constants for drawing calculations
+    max_label_width <- max(unlist(lapply(taxa, function(taxon) return(nchar(taxon$name)))))
+    drawing_width <- column_width - max_label_width - 1
+
+    return(list())
+  }
+
+  get_row_positions <- function(tree, taxa) {
+    positions <- lapply(1:length(taxa), function(id) return(2*(id-1)))
+    names(positions) <- lapply(taxa, function(taxon) return(taxon$name))
+
+    return(list())
+  }
+
+  clean_tree <- organize_tree(tree)
+  taxa <- get_terminals(tree)
+  col_positions <- get_col_positions(tree, taxa)
+  row_positions <- get_row_positions(tree, taxa)
+
+  nodes <- list()
+  edges <- list()
+
+  # TODO: remove dummy data to not overwrite outputs using functions above
+  nodes <- list(1,2,3)
+  edges <- list(4,5,6)
+  elements <- c(nodes, edges)
+
+  return (elements)
+}
+
+elements <- generate_elements(tree)
 
 layout <- list('name' = 'preset')
 
@@ -53,7 +209,7 @@ app$layout(
     list(
       cytoCytoscape(
         id = 'cytoscape',
-        elements = list(), # change to 'elements' once defined
+        elements = list(), # TODO: change to 'elements' once defined
         stylesheet = stylesheet,
         layout = layout,
         style = list(
@@ -80,4 +236,4 @@ app$callback(
   }
 )
 
-#app$run_server()
+# app$run_server()
