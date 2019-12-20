712cba801d75c11f0ef543489c943b08cfe790aa
Ryan Patrick Kyle <ryan@plot.ly>
2019-12-19 22:39:30 -0500

    :package: update R package to 0.1.1

--- R/cytoCytoscape.R
+++ R/cytoCytoscape.R
@@ -2,16 +2,17 @@
 
 cytoCytoscape <- function(id=NULL, className=NULL, style=NULL, elements=NULL, stylesheet=NULL, layout=NULL, pan=NULL, zoom=NULL, panningEnabled=NULL, userPanningEnabled=NULL, minZoom=NULL, maxZoom=NULL, zoomingEnabled=NULL, userZoomingEnabled=NULL, boxSelectionEnabled=NULL, autoungrabify=NULL, autolock=NULL, autounselectify=NULL, autoRefreshLayout=NULL, tapNode=NULL, tapNodeData=NULL, tapEdge=NULL, tapEdgeData=NULL, mouseoverNodeData=NULL, mouseoverEdgeData=NULL, selectedNodeData=NULL, selectedEdgeData=NULL) {
     
+    props <- list(id=id, className=className, style=style, elements=elements, stylesheet=stylesheet, layout=layout, pan=pan, zoom=zoom, panningEnabled=panningEnabled, userPanningEnabled=userPanningEnabled, minZoom=minZoom, maxZoom=maxZoom, zoomingEnabled=zoomingEnabled, userZoomingEnabled=userZoomingEnabled, boxSelectionEnabled=boxSelectionEnabled, autoungrabify=autoungrabify, autolock=autolock, autounselectify=autounselectify, autoRefreshLayout=autoRefreshLayout, tapNode=tapNode, tapNodeData=tapNodeData, tapEdge=tapEdge, tapEdgeData=tapEdgeData, mouseoverNodeData=mouseoverNodeData, mouseoverEdgeData=mouseoverEdgeData, selectedNodeData=selectedNodeData, selectedEdgeData=selectedEdgeData)
+    if (length(props) > 0) {
+        props <- props[!vapply(props, is.null, logical(1))]
+    }
     component <- list(
-        props = list(id=id, className=className, style=style, elements=elements, stylesheet=stylesheet, layout=layout, pan=pan, zoom=zoom, panningEnabled=panningEnabled, userPanningEnabled=userPanningEnabled, minZoom=minZoom, maxZoom=maxZoom, zoomingEnabled=zoomingEnabled, userZoomingEnabled=userZoomingEnabled, boxSelectionEnabled=boxSelectionEnabled, autoungrabify=autoungrabify, autolock=autolock, autounselectify=autounselectify, autoRefreshLayout=autoRefreshLayout, tapNode=tapNode, tapNodeData=tapNodeData, tapEdge=tapEdge, tapEdgeData=tapEdgeData, mouseoverNodeData=mouseoverNodeData, mouseoverEdgeData=mouseoverEdgeData, selectedNodeData=selectedNodeData, selectedEdgeData=selectedEdgeData),
+        props = props,
         type = 'Cytoscape',
         namespace = 'dash_cytoscape',
         propNames = c('id', 'className', 'style', 'elements', 'stylesheet', 'layout', 'pan', 'zoom', 'panningEnabled', 'userPanningEnabled', 'minZoom', 'maxZoom', 'zoomingEnabled', 'userZoomingEnabled', 'boxSelectionEnabled', 'autoungrabify', 'autolock', 'autounselectify', 'autoRefreshLayout', 'tapNode', 'tapNodeData', 'tapEdge', 'tapEdgeData', 'mouseoverNodeData', 'mouseoverEdgeData', 'selectedNodeData', 'selectedEdgeData'),
         package = 'dashCytoscape'
         )
 
-    component$props <- filter_null(component$props)
-
     structure(component, class = c('dash_component', 'list'))
 }
-
--- R/internal.R
+++ R/internal.R
@@ -2,13 +2,7 @@
 deps_metadata <- list(`dash_cytoscape` = structure(list(name = "dash_cytoscape",
 version = "0.1.1", src = list(href = NULL,
 file = "deps"), meta = NULL,
-script = "dash_cytoscape.min.js",
-stylesheet = NULL, head = NULL, attachment = NULL, package = "dashCytoscape",
-all_files = FALSE), class = "html_dependency"),
-`dash_cytoscape_dev` = structure(list(name = "dash_cytoscape",
-version = "0.1.1", src = list(href = NULL,
-file = "deps"), meta = NULL,
-script = "dash_cytoscape.dev.js",
+script = 'dash_cytoscape.min.js',
 stylesheet = NULL, head = NULL, attachment = NULL, package = "dashCytoscape",
 all_files = FALSE), class = "html_dependency"))
 return(deps_metadata)
