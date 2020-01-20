a52a7f0131d3705ae6e4e84c1a16c786d7396b3a
Marc-André Rivet <marcandre.rivet@sociablelabs.com>
2020-01-20 14:25:29 -0500

    lint

--- tests/test_callbacks.py
+++ tests/test_callbacks.py
@@ -6,7 +6,6 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.keys import Keys
-import time
 
 class Tests(IntegrationTests):
     def test_callbacks(self):
