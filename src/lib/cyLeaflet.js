984a8e1983008807c0f59a46969623865e781011
Manfred Cheung <mj3cheun@uwaterloo.ca>
2021-09-24 13:51:29 -0400

    Poll cytoscape container and overlay the map on top of the cytoscape container

--- src/lib/cyLeaflet.js
+++ src/lib/cyLeaflet.js
@@ -2,12 +2,15 @@ export default class cyLeaflet {
     constructor(cy) {
         this.updateCtxmenu = this.update.bind(this);
         this.initializeLeaflet = this.initializeLeaflet.bind(this);
+        this.generateContainerStyle = this.generateContainerStyle.bind(this);
+        this.addLeafletTiles = this.addLeafletTiles.bind(this);
 
         this.cy = cy;
         this.setProps = null;
         this.leafletContainer = null;
         this.leafletInstance = null;
         this.leafletHash = '';
+        this.leafletContainerUpdateInterval = null;
     }
 
     update(props) {
@@ -18,25 +21,38 @@ export default class cyLeaflet {
         }
 
         const leafletHashNew = JSON.stringify(leaflet);
-        if(leafletHashNew !== this.leafletHash) {
+        if(this.leafletInstance) {
+            this.leafletInstance.destroy();
+            clearInterval(this.leafletContainerUpdateInterval);
+        }
+        if(leafletHashNew !== this.leafletHash && leafletHashNew) {
             this.initializeLeaflet();
             this.addLeafletTiles(props);
             this.leafletHash = leafletHashNew;
         }
     }
 
-    initializeLeaflet() {
-        if(this.leafletInstance) {
-            this.leafletInstance.destroy();
-
+    generateContainerStyle() {
+        if(this.cy) {
+            const {top, left, height, width} = this.cy.container().getBoundingClientRect();
+            return `position: fixed; z-index: 0; top: ${top}px; left: ${left}px; height: ${height}px; width: ${width}px;`;
         }
+        else {
+            return '';
+        }
+    }
 
+    initializeLeaflet() {
         if(!this.leafletContainer) {
             this.leafletContainer = document.createElement('div');
             this.leafletContainer.setAttribute('class', 'cy-leaflet-container');
-            this.leafletContainer.setAttribute('style', 'right: 0; bottom: 0; position: absolute; left: 0px; top: 0px; z-index: 0;');
+            this.leafletContainer.setAttribute('style', this.generateContainerStyle());
             this.cy.container().style.zIndex = 1;
             this.cy.container().parentNode.appendChild(this.leafletContainer);
+
+            this.leafletContainerUpdateInterval = setInterval(() => {
+                this.leafletContainer.setAttribute('style', this.generateContainerStyle());
+            }, 200);
         }
     }
 
