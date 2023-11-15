d0570b023b3d803917dd3f549ab3f2c41b1fb070
Clea Aumont <clea.aumont@plot.ly>
2023-11-15 18:45:06 +0100

    update namespace function  declaration

--- demos/assets/ctxMenuFns.js
+++ demos/assets/ctxMenuFns.js
@@ -1,25 +1,31 @@
 window.dashCytoscapeFunctions = {};
 
-window.dashCytoscapeFunctions.add_2_nodes = function (event) {
-    var pos = event.position || event.cyPosition;
-    cy.add({
-        data: {
-            group: 'nodes',
-            id: Date.now(),
+window.dashCytoscapeFunctions = Object.assign(
+    {},
+    window.dashCytoscapeFunctions,
+    {
+        add_2_nodes: function (event) {
+            var pos = event.position || event.cyPosition;
+            cy.add({
+                data: {
+                    group: 'nodes',
+                    id: Date.now(),
+                },
+                position: {
+                    x: pos.x,
+                    y: pos.y,
+                },
+            });
+            cy.add({
+                data: {
+                    group: 'nodes',
+                    id: `${Date.now()}_2`,
+                },
+                position: {
+                    x: pos.x + 50,
+                    y: pos.y + 50,
+                },
+            });
         },
-        position: {
-            x: pos.x,
-            y: pos.y,
-        },
-    });
-    cy.add({
-        data: {
-            group: 'nodes',
-            id: `${Date.now()}_2`,
-        },
-        position: {
-            x: pos.x + 50,
-            y: pos.y + 50,
-        },
-    });
-};
+    }
+);
