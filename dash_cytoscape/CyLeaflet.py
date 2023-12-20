20fb52626267948e3ffec8ea8a60391ae7e87de8
Emily Kellison-Linn <4672118+emilykl@users.noreply.github.com>
2023-12-19 19:05:38 -0500

    improved init logic for CyLeaflet

--- dash_cytoscape/CyLeaflet.py
+++ dash_cytoscape/CyLeaflet.py
@@ -13,35 +13,11 @@ import dash_leaflet as dl
 
 class CyLeaflet(html.Div):
     def __init__(self, id, cytoscape_props=None, leaflet_props=None):
-        cytoscape_props = cytoscape_props or {}
-        leaflet_props = leaflet_props or {}
-        elements = cytoscape_props.get("elements", [])
         self.ids = {s: {"id": id, "sub": s} for s in ["cy", "leaf", "elements"]}
-        cytoscape_props = dict(
-            cytoscape_props,
-            id=self.ids["cy"],
-            elements=[],
-            boxSelectionEnabled=True,
-            layout={"name": "preset"},
-            style={
-                "width": "100%",
-                "height": "100%",
-            },
-            maxZoom=5,
-            minZoom=3 / 100000,
-        )
-        leaflet_props = dict(
-            leaflet_props,
-            id=self.ids["leaf"],
-            children=dl.TileLayer(),
-            zoomSnap=0,
-            zoomControl=False,
-            zoomAnimation=False,
-            inertia=False,
-            maxZoom=100000,
-            maxBoundsViscosity=1,
-            maxBounds=[[-85, -180.0], [85, 180.0]],
-            style={"width": "100%", "height": "100%"},
+        elements = cytoscape_props.get("elements", [])
+
+        cytoscape_props, leaflet_props = self.set_default_props_and_overrides(
+            cytoscape_props, leaflet_props
         )
 
         super().__init__(
@@ -77,6 +53,55 @@ class CyLeaflet(html.Div):
             },
         )
         self.add_clientside_callbacks()
+
+    def set_default_props_and_overrides(self, user_cytoscape_props, user_leaflet_props):
+        user_cytoscape_props = user_cytoscape_props or {}
+        user_leaflet_props = user_leaflet_props or {}
+
+        # Props where we want to override values supplied by the user
+        # These are props which are required for CyLeaflet to work properly
+        cytoscape_overrides = {
+            "id": self.ids["cy"],
+            "elements": [],  # Elements are set via clientside callback, so set to empty list initially
+            "layout": {"name": "preset"},
+            "style": {"width": "100%", "height": "100%"},
+            "maxZoom": 5,
+            "minZoom": 3 / 100000,
+        }
+        leaflet_overrides = {
+            "id": self.ids["leaf"],
+            "zoomSnap": 0,
+            "zoomControl": False,
+            "zoomAnimation": False,
+            "inertia": False,
+            "maxZoom": 100000,
+            "maxBoundsViscosity": 1,
+            "maxBounds": [[-85, -180.0], [85, 180.0]],
+            "style": {"width": "100%", "height": "100%"},
+        }
+
+        # Props where we want to fill in a default value
+        # if a value is not supplied by the user
+        cytoscape_defaults = {
+            "boxSelectionEnabled": True,
+        }
+        leaflet_defaults = {
+            "children": dl.TileLayer(),
+        }
+
+        # Start with default props
+        cytoscape_props = dict(cytoscape_defaults)
+        leaflet_props = dict(leaflet_defaults)
+
+        # Update with user-supplied props
+        cytoscape_props.update(user_cytoscape_props)
+        leaflet_props.update(user_leaflet_props)
+
+        # Update with overrides
+        cytoscape_props.update(cytoscape_overrides)
+        leaflet_props.update(leaflet_overrides)
+
+        return cytoscape_props, leaflet_props
 
     def add_clientside_callbacks(self):
         clientside_callback(
