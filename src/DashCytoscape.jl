96834f745961d4667e6f671a8c75381ca82d5dfe
alexcjohnson <alex@plot.ly>
2024-01-26 20:29:08 -0500

    v1.0.0

--- src/DashCytoscape.jl
+++ src/DashCytoscape.jl
@@ -3,7 +3,7 @@ module DashCytoscape
 using Dash
 
 const resources_path = realpath(joinpath( @__DIR__, "..", "deps"))
-const version = "0.3.0"
+const version = "1.0.0"
 
 include("jl/cyto_cytoscape.jl")
 
@@ -16,7 +16,7 @@ function __init__()
             [
                 DashBase.Resource(
     relative_package_path = "dash_cytoscape.min.js",
-    external_url = "https://unpkg.com/dash-cytoscape@0.3.0/dash_cytoscape/dash_cytoscape.min.js",
+    external_url = "https://unpkg.com/dash-cytoscape@1.0.0/dash_cytoscape/dash_cytoscape.min.js",
     dynamic = nothing,
     async = nothing,
     type = :js
